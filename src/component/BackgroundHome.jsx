204961a721f4d950636c4507186ca71ade679a7b
Freddy <freddyvelarde59@gmail.com>
2021-09-20 19:51:31 -0400

    I made the 'CV button!

--- src/component/BackgroundHome.jsx
+++ src/component/BackgroundHome.jsx
@@ -15,7 +15,11 @@ function BackgroundHome() {
           <h3>
             I work with the most popular and demanded technologies and I am ready to work with you.  
           </h3>
+          <div className="buttons-home">
           <a href="#about" className="button-link">Let's started</a>
+          <a href="https://drive.google.com/file/d/12_3ABx3NJyFxiHa5onRc-3Cez07zKmw4/view" id="cv"target="__blank" className="button-link">Download CV</a>
+
+          </div>
         </div>
         
       </div>
